a2ddb5089b743a13c05add3d6f62aa7a83d96420
MaciejBodnar <maciej.bodnar2@gmail.com>
2024-05-28 10:35:07 +0200

    adding files, config, styling

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <main className="container-main flex flex-col justify-evenly items-center">
+        <h1 className="text-5xl font-bold">Tenzies</h1>
+        <p className="w-4/6 text-center -mt-8 text-lg">
+          Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
+        </p>
+        <div className="dices w-5/6 h-48 rounded-xl bg-slate-300"></div>
+        <button className="self-center px-14 py-2 bg-blue-600 text-white text-xl rounded-lg">Roll</button>
+      </main>
+    </>
+  )
+}
+
+export default App
