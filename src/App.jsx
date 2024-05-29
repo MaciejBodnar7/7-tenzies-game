091447bcc1510611d816334003e9749b6b366f46
MaciejBodnar <maciej.bodnar2@gmail.com>
2024-05-29 10:46:59 +0200

    generating num in obj, add nanoid for key, btn roll new die

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import React from "react"
 import "./App.css"
 import Die from "./Die"
+import { nanoid } from "nanoid"
 
-function App() {
+export default function App() {
+  const [dice, setDice] = React.useState(allNewDice)
+
+  const diceElement = dice.map(die => {
+    return <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+  })
+
+  function allNewDice() {
+    const arr = []
+    while (arr.length < 10) {
+      const randomNumb = Math.floor(Math.random() * 6) + 1
+      arr.push({ id: nanoid(), value: randomNumb, isHeld: false })
+    }
+    return arr
+  }
+
+  function rollDice() {
+    setDice(allNewDice)
+  }
+
   return (
     <>
       <main className="container-main flex flex-col justify-evenly items-center">
@@ -9,22 +30,11 @@ function App() {
         <p className="w-4/6 text-center -mt-8 text-lg">
           Roll until all dice are the same. Click each die to freeze it at its current value between rolls.
         </p>
-        <div className="dices w-5/6 h-48 rounded-xl grid grid-cols-5 gap-6">
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-          <Die value={1} />
-        </div>
-        <button className="self-center px-14 py-2 bg-blue-600 text-white text-xl rounded-lg">Roll</button>
+        <div className="dices w-5/6 h-48 rounded-xl grid grid-cols-5 gap-6">{diceElement}</div>
+        <button onClick={rollDice} className="self-center px-14 py-2 bg-blue-600 text-white text-xl rounded-lg">
+          Roll
+        </button>
       </main>
     </>
   )
 }
-
-export default App
